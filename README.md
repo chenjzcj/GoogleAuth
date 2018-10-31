2a7a9e1479d92e477e8b10573c0ffca4d6f5edd1
felix.zhong <527633405@qq.com>
2018-10-31 14:57:31 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+###谷歌验证生成器
+参考博客：
+1. Java 接入 Google Authenticator（主要实现代码参考此博客）
+https://www.jianshu.com/p/de903c074d77
+2. Google Authenticator JAVA实例
+http://awtqty-zhang.iteye.com/blog/1986275
+https://www.cnblogs.com/Athrun/p/php-google-authenticator.html
+3. Google Authenticator 如何集成（U盾的实现原理相同）
+https://www.cnblogs.com/taiyonghai/p/6831385.html
+4. 不懂使用Google身份验证器你就亏大了
+https://www.jianshu.com/p/f268b9f41390
--- app/src/main/java/com/felix/googleauth/googleauth/GoogleAuthHelper.java
+++ app/src/main/java/com/felix/googleauth/googleauth/GoogleAuthHelper.java
@@ -1,7 +1,108 @@
 package com.felix.googleauth.googleauth;
 
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Hex;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+
 /**
  * Created by Felix.Zhong on 2018/10/31 14:13
  */
 public class GoogleAuthHelper {
+
+    public static void main(String[] args) {
+        //String secretKey = createSecretKey();
+        String secretKey = "qxkiwlceownnciyhk2vgkyv73ferxsf3";
+        //1970-01-01 00:00:00 以来的毫秒数除以 30
+        long time = System.currentTimeMillis() / 1000 / 30;
+        String totp = getTOTP(secretKey, time);
+
+        System.out.println("secretKey = " + secretKey + ",time = " + time + ",totp = " + totp);
+    }
+
+    /**
+     * 随机生成一个密钥(小写)
+     */
+    public static String createSecretKey() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[20];
+        random.nextBytes(bytes);
+        Base32 base32 = new Base32();
+        String secretKey = base32.encodeToString(bytes);
+        return secretKey.toLowerCase();
+    }
+
+    /**
+     * 随机生成一个密钥(大写)
+     */
+    public static String createSecretKeyUpperCase() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[20];
+        random.nextBytes(bytes);
+        Base32 base32 = new Base32();
+        String secretKey = base32.encodeToString(bytes);
+        return secretKey.toUpperCase();
+    }
+
+
+    /**
+     * 根据密钥获取验证码
+     * 返回字符串是因为验证码有可能以 0 开头
+     *
+     * @param secretKey 密钥
+     * @param time      第几个 30 秒 System.currentTimeMillis() / 1000 / 30
+     */
+    public static String getTOTP(String secretKey, long time) {
+        Base32 base32 = new Base32();
+        byte[] bytes = base32.decode(secretKey.toUpperCase());
+        String hexKey = Hex.encodeHexString(bytes);
+        String hexTime = Long.toHexString(time);
+        return TOTP.generateTOTP(hexKey, hexTime, "6");
+    }
+
+
+    /**
+     * 生成 Google Authenticator 二维码所需信息
+     * Google Authenticator 约定的二维码信息格式 : otpauth://totp/{issuer}:{account}?secret={secret}&issuer={issuer}
+     * 参数需要 url 编码 + 号需要替换成 %20
+     *
+     * @param secret  密钥 使用 createSecretKey 方法生成
+     * @param account 用户账户 如: example@domain.com 138XXXXXXXX
+     * @param issuer  服务名称 如: Google Github 印象笔记
+     */
+    public static String createGoogleAuthQRCodeData(String secret, String account, String issuer) {
+        String qrCodeData = "otpauth://totp/%s?secret=%s&issuer=%s";
+        try {
+            return String.format(qrCodeData, URLEncoder.encode(issuer + ":" + account, "UTF-8").replace("+", "%20"), URLEncoder.encode(secret, "UTF-8")
+                    .replace("+", "%20"), URLEncoder.encode(issuer, "UTF-8").replace("+", "%20"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+    /**
+     * 时间前后偏移量
+     */
+    private static final int timeExcursion = 3;
+
+    /**
+     * 校验方法
+     *
+     * @param secretKey 密钥
+     * @param code      用户输入的 TOTP 验证码
+     */
+    public static boolean verify(String secretKey, String code) {
+        long time = System.currentTimeMillis() / 1000 / 30;
+        for (int i = -timeExcursion; i <= timeExcursion; i++) {
+            String totp = getTOTP(secretKey, time + i);
+            if (code.equals(totp)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
