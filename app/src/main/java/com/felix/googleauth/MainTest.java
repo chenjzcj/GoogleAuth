35efcb71c3be91628d0ccc00b228555a3e3f010e
felix.zhong <527633405@qq.com>
2018-11-07 11:14:37 +0800

    1.成功发布版本1.0.2

--- app/src/main/java/com/felix/googleauth/MainTest.java
+++ app/src/main/java/com/felix/googleauth/MainTest.java
@@ -10,6 +10,8 @@ public class MainTest {
 
     public static void main(String[] args) {
         String googleAuthQRCodeData = GoogleAuthHelper.createGoogleAuthQRCodeData("aaaaaaaaaaaaaa", "54246346", "TOKOK");
-        System.out.print("googleAuthQRCodeData = " + googleAuthQRCodeData);
+        String googleAuthQRCodeStr = GoogleAuthHelper.createGoogleAuthQRCodeStr("ASDFASDFASDFASDF", "352625262@qq.com", "HUOBI");
+        System.out.println("googleAuthQRCodeData = " + googleAuthQRCodeData);
+        System.out.println("googleAuthQRCodeStr = " + googleAuthQRCodeStr);
     }
 }
