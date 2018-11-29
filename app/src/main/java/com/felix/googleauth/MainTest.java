72a8d0726fc943f5fc52b417c94c9554565ff21c
felix.zhong <527633405@qq.com>
2018-11-29 09:48:02 +0800

    add example

--- app/src/main/java/com/felix/googleauth/MainTest.java
+++ app/src/main/java/com/felix/googleauth/MainTest.java
@@ -11,6 +11,8 @@ public class MainTest {
     public static void main(String[] args) {
         String googleAuthQRCodeData = GoogleAuthHelper.createGoogleAuthQRCodeData("aaaaaaaaaaaaaa", "54246346", "TOKOK");
         String googleAuthQRCodeStr = GoogleAuthHelper.createGoogleAuthQRCodeStr("ASDFASDFASDFASDF", "352625262@qq.com", "HUOBI");
+        boolean asergswergasdfg5 = GoogleAuthHelper.verifyNoExcursion("ASERGSWERGASDFG5", "907798");
+        System.out.println("asergswergasdfg5 = " + asergswergasdfg5);
         System.out.println("googleAuthQRCodeData = " + googleAuthQRCodeData);
         System.out.println("googleAuthQRCodeStr = " + googleAuthQRCodeStr);
     }
